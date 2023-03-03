fa9a9517c77eb9121d9107229baaeedc572627ca
madame3369 <akeka0303@naver.com>
2023-03-03 15:31:10 +0900

    코드commit 습관들이기 1일차

---
+++ 몫 구하기.js
@@ -0,0 +1,15 @@
+function solution(num1, num2){
+    var answer= ~~(num1/num2);
+    return answer;
+}
+console.log(solution(12,3));
+
+// var answer = num1/num2;
+// return Math.floor(answer);
+
+// var answer = parseInt(num1/num2);
+// return answer;
+
+// return Math.trunc(num1/num2);
+
+// return (num1/num2)<<0;
---
+++ 숫자 비교하기.js
@@ -0,0 +1,13 @@
+function solution(num1, num2){
+    return num1 === num2 ? 1 : -1;
+}
+console.log(solution(2,2));
+console.log(solution(2,3));
+
+// let answer = 0;
+// if(num1===num2){
+//     answer = 1;
+// }else{
+//     answer = -1;
+// }
+// return answer;
